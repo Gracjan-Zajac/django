42d9a5a7508f2203943da3b730b25e72a27fdc07
gracjan-zajac <gracjan.zajac@hotmail.com>
2022-10-02 10:14:35 +0200

    Title slug

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=150)
+    slug = models.SlugField(null=True, blank=True)
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.DateField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.slug is None:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
