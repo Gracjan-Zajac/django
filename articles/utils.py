f9295f601a4154239655bbd987dd1970c9cdaaf5
gracjan-zajac <gracjan.zajac@hotmail.com>
2022-10-05 10:35:32 +0200

    slug in dynamic url

--- articles/utils.py
+++ articles/utils.py
@@ -3,8 +3,8 @@ from django.utils.text import slugify
 
 def slugify_instance_title(instance, save=False):
     slug = slugify(instance.title)
-    Klass = instance.__class__
-    qs = Klass.objects.filter(slug=slug).exclude(id=instance.id)
+    Class = instance.__class__
+    qs = Class.objects.filter(slug=slug).exclude(id=instance.id)
     if qs.exists():
         slug = f"{slug}-{instance.id}"
     instance.slug = slug
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import Http404
 
 from .forms import ArticleForm
 from .models import Article
@@ -30,8 +31,16 @@ def article_create_view(request):
     return render(request, "articles/create.html", context=context)
 
 
-def article_detail_view(request, id=None):
-    if id is not None:
-        article_obj = Article.objects.get(id=id)
+def article_detail_view(request, slug=None):
+    article_obj = None
+    if slug is not None:
+        try:
+            article_obj = Article.objects.get(slug=slug)
+        except Article.DoesNotExist:
+            raise Http404("Article does not exist")
+        except Article.MultipleObjectsReturned:
+            article_obj = Article.objects.filter(slug=slug).first()
+        except:
+            raise Http404
     context = {"object": article_obj}
     return render(request, "articles/detail.html", context=context)
