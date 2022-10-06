fc8846eb93e01fea504506b333ed8500ea6a673c
gracjan-zajac <gracjan.zajac@hotmail.com>
2022-10-06 12:35:56 +0200

    Recipes added to admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Recipe, RecipeIngredient
 
 # Register your models here.
+
+
+class RecipeIngredientInLine(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["name", "user"]
+    readonly_fields = ["timestamp", "updated"]
+    raw_id_fields = ["user"]
+    inlines = [RecipeIngredientInLine]
+
+
+admin.site.register(Recipe, RecipeAdmin)
